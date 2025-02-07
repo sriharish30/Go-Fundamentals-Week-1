cd063fe0f8d1e60ed2695a3f10078c122a758981
sri harish <sriharish9626@gmail.com>
2025-02-07 15:17:48 +0530

    Declaring the variables and print both types and values

---
+++ variables.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 12
+	b := 123.78
+	c := true
+	d := "sriharish"
+	fmt.Printf("value: %v, type: %T\n", a, a)
+	fmt.Printf("value: %v, type: %T\n", b, b)
+	fmt.Printf("value: %v, type: %T\n", c, c)
+	fmt.Printf("value: %v, type: %T\n", d, d)
+
+}

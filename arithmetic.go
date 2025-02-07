c6deb4c5a5f2737f3c3abbbe054e50b6df7ebc28
sri harish <sriharish9626@gmail.com>
2025-02-07 15:09:35 +0530

    Implement Arithmetic Operations

---
+++ arithmetic.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	b := 5
+	fmt.Println("sum: ", a+b)
+	fmt.Println("diffence: ", a-b)
+	fmt.Println("product: ", a*b)
+	fmt.Println("quotient: ", a/b)
+
+}
